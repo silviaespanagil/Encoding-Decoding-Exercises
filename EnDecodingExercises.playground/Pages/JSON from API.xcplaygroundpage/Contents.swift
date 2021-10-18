4680b15f5738f0cc2b54d4896f231952469768ab
Silvia España <silviaespanagil@gmail.com>
2021-10-18 15:02:45 +0200

    Add managing simple api exercise - GitHub api

--- EnDecodingExercises.playground/Pages/JSON from API.xcplaygroundpage/Contents.swift
+++ EnDecodingExercises.playground/Pages/JSON from API.xcplaygroundpage/Contents.swift
@@ -1,7 +1,37 @@
-//: [Previous](@previous)
 
 import Foundation
+import UIKit
 
-var greeting = "Hello, playground"
+struct gitFollower: Decodable {
+    let login: String
+    let id: Int
+}
 
-//: [Next](@next)
+//funcion to retrieve data
+
+func getJSON() {
+    guard let url = URL(string: "https://api.github.com/users/silviaespanagil/followers") else {
+        return
+    }
+    let request = URLRequest(url: url)
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        guard let data = data else {
+            return
+        }
+        
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode([gitFollower].self, from: data) else {
+            return
+        }
+        let followers = decodedData
+        for follower in followers {
+            print(follower.login)
+        }
+    }.resume()
+}
+
+getJSON()
